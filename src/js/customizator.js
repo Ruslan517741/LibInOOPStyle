81a2b4256924b78a2cd82eb34cadce0ef3525785
Rus <mamail>
2021-09-23 16:50:26 +0300

    Start project

---
+++ src/js/customizator.js
@@ -0,0 +1,14 @@
+export default class Customizaror {
+    constructor() {
+
+    }
+
+
+    render() {
+        this.btnBlock = document.createElement('div');
+        let scaleInputsS = document.createElement('input'),
+            scaleInputsM = document.createElement('input');
+
+        console.log(this.btnBlock, scaleInputsS, scaleInputsM);
+    }
+}
